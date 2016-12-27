f4ffd4bc1e165ca966b4efaa3bddda2cdaccd86c
LechuckRoh <member@lechuck2011.cafe24.com>
2016-12-27 22:03:52 +0900

    gocraft/web sample

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gocraft/web"
+)
+
+type User struct {
+	Name string
+}
+
+type Context struct {
+	User *User
+}
+
+func (c *Context) UsersList(rw web.ResponseWriter, req *web.Request) {
+	fmt.Fprint(rw, "users")
+}
+
+func (c *Context) Root(rw web.ResponseWriter, req *web.Request) {
+	if c.User != nil {
+		fmt.Fprint(rw, "Hello ", c.User.Name)
+	} else {
+		fmt.Fprint(rw, "Hello guest")
+	}
+}
+
 func main() {
-    println("hello")
+	router := web.New(Context{})
+	router.Get("/users", (*Context).UsersList)
+	router.Get("/", (*Context).Root)
+	http.ListenAndServe("localhost:3000", router)
 }
